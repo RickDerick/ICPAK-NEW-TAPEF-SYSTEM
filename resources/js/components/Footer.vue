d7293e52f780799cfbc15e5c27cc44119c605ca9
RickDerick <RickDerick>
2026-06-10 14:09:04 +0300

    updates on auth module

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-footer app class="bg-primary text-white">
-    <v-container class="d-flex justify-space-between align-center pa-0">
+  <v-footer class="bg-primary text-white fixed-footer">
+    <v-container class="d-flex justify-space-between align-center pa-3">
 
       <div v-if="company">
         <a
@@ -41,3 +41,25 @@ const company = ref({
 
 const year = computed(() => new Date().getFullYear())
 </script>
+
+<style scoped>
+.fixed-footer {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 72px;
+  z-index: 1400;
+}
+
+.fixed-footer .v-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+/* Safe area support for iOS */
+.fixed-footer {
+  padding-bottom: env(safe-area-inset-bottom);
+}
+</style>
